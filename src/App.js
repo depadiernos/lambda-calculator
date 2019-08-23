bb88cffc0a83b04e68138a3a4fbaf60e2800722d
Dennis Padiernos <depadiernos@gmail.com>
2019-08-23 17:18:48 -0600

    implement mvp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
-// STEP 4 - import the button and display components
-// Don't forget to import any extra css/scss files you build into the correct component
-
-// Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import Display from './components/DisplayComponents/Display'
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers'
+import Operator from './components/ButtonComponents/OperatorButtons/Operators'
+import Special from './components/ButtonComponents/SpecialButtons/Specials'
+
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -17,7 +18,14 @@ function App() {
     <div className="container">
       <Logo />
       <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display>0</Display>
+        <div className="button-container">
+          <div className="calc">
+            <Special />
+            <Numbers />
+          </div>
+          <Operator />
+        </div>
       </div>
     </div>
   );
